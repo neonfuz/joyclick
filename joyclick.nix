851ea54fea10d1b81f299acb50835110c99898a2
Sage Raflik <neonfuz@gmail.com>
2019-09-18 00:56:42 -0500

    Nix package cleanup

--- joyclick.nix
+++ joyclick.nix
@@ -12,13 +12,9 @@ stdenv.mkDerivation rec {
 #    sha256 = "0jj67yp00y8mnxys37xb39dq442k3xsxm2nd67vvggq0k8rqp2p2";
 #  };
 
-  nativeBuildInputs = [
-    makeWrapper
-  ];
+  nativeBuildInputs = [ makeWrapper ];
 
-  buildInputs = [
-    SDL2 xdotool
-  ];
+  buildInputs = [ SDL2 xdotool ];
 
   installPhase = ''
     mkdir $out
@@ -26,10 +22,8 @@ stdenv.mkDerivation rec {
     wrapProgram $out/bin/joyclick --prefix PATH ":" ${xdotool}/bin ;
   '';
 
-  #  doCheck = true;
-
   meta = with stdenv.lib; {
-     description = "Program to click places with game controller";
+    description = "Program to click places with game controller";
     longDescription = ''
       Click defined locations on your screen with game controller
       button presses.
